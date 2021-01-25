e41c9006df09971bfcf4e5802c835213422d1f07
庚什么码 <72619518+chengeng-GC@users.noreply.github.com>
2021-01-25 17:25:40 +0800

    Add files via upload

---
+++ game1.h
@@ -0,0 +1,12 @@
+#define ROW 3
+#define COL 3
+//ÉùÃ÷
+void InitBoard(char board[ROW][COL],int row,int col);
+
+void DisplayBoard(char board[ROW][COL], int row, int col);
+
+void PlayerMove(char board[ROW][COL], int row, int col);
+
+void ComputerMove(char board[ROW][COL], int row, int col);
+
+char IsWin(char board[ROW][COL], int row, int col);
